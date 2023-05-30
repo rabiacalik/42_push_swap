adb572d00bcd261b0a92005e0590c89b6395e5a1
Rabia Calik <115617420+rabiacalik@users.noreply.github.com>
2023-05-30 09:22:25 +0300

    add ft_short_three

--- reverse.c
+++ reverse.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-int reverse_rotate(t_list **stack)
+int reverse_rotate(t_list **stack) //??
 {
     t_list *tmp;
     t_list *end;
@@ -30,5 +30,27 @@ int rra(t_list **stack_a)
     if (reverse_rotate(stack_a) == -1)
         return (-1);
     
+    ft_putndl_fd("rrb", 1);
+    return (0);
+}
+
+int rrb(t_list **stack_b)
+{
+    if (reverse_rotate(stack_b) == -1)
+        return (-1);
     
+    ft_putndl_fd("rrb", 1);
+    return (0);
+}
+
+int rrr(t_list **stack_a, t_list **stack_b)
+{
+    if ((ft_lstsize(*stack_a) < 2) || (ft_lstsize(*stack_b) < 2))
+        return (-1);
+    
+    reverse_rotate(stack_a);
+    reverse_rotate(stack_b);
+
+    ft_putndl_fd("rrr", 1);
+    return (0);
 }
