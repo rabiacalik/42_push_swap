bd6d10596404f5c7d5170d058d0e8bee456c68f8
Rabia Calik <115617420+rabiacalik@users.noreply.github.com>
2023-05-30 09:31:25 +0300

    as

---
+++ rotate.c
@@ -0,0 +1,9 @@
+#include "push_swap.h"
+
+int rotate(t_list *stack)
+{
+    t_list *tmp;
+    t_list *end;
+
+    if (ft_lstsize(*stack))
+}
--- simple.c
+++ simple.c
@@ -2,7 +2,7 @@
 
 void ft_small_sort(t_list **stack_a, t_list **stack_b)
 {
-    (void)stack_b;
+    (void)stack_b; //neden böyle yaptık
 
     if (ft_lstsize(*stack_a) == 2) //liste boyutu 2 ye eşitse
     {
@@ -13,7 +13,11 @@ void ft_small_sort(t_list **stack_a, t_list **stack_b)
     else if (ft_lstsize(*stack_a) == 3) // boyutu 3 e eşitse
         ft_sort_three(stack_a); 
 
-    
+    else if (ft_lstsize(*stack_a) == 4)
+        ft_sort_four(stack_a);
+
+    else if (ft_lstsize(stack_a))
+        ft_sort_five(stack_a, stack_b);
 }
 
 /*
